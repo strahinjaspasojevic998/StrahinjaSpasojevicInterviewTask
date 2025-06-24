dbc89365ee6fd70c919df2c0592ea5e94223342f
Strahinja Spasojevic <strahinja.spasojevic998@gmail.com>
2025-06-24 02:23:10 +0200

    initial commit

---
+++ Selenium/TaskForInterview/TaskForInterview/CartPageTest.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.ObjectModel;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TaskForInterview
+{
+    [TestFixture]
+    internal class CartPageTest
+    {
+        public string url = "https://www.saucedemo.com/";
+        public IWebDriver driver;
+        //elements
+        public string shoppingCartBadgeMessageXPath = "//div[@id='shopping_cart_container']/a[@class='shopping_cart_link']/span[@class='shopping_cart_badge']";
+        public string buttonForAddingItemSauceLabsBackpackId = "add-to-cart-sauce-labs-backpack";
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--incognito");
+            this.driver = new ChromeDriver(options);
+            this.driver.Manage().Window.Maximize();
+            this.driver.Navigate().GoToUrl(url);
+            this.LogIn();
+
+        }
+
+        private void LogIn()
+        {
+            this.driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            this.driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            this.driver.FindElement(By.Id("login-button")).Click();
+            string currentURL = this.driver.Url;
+            Assert.That(currentURL, Is.EqualTo("https://www.saucedemo.com/inventory.html"), "URL is not correct.");
+        }
+        
+        [TearDown]
+        public void TearDown()
+        {
+            this.driver.Close();
+        }
+        [Test]
+
+        public void TestAddingOneItemInCart()
+        {
+
+            this.driver.FindElement(By.Id(buttonForAddingItemSauceLabsBackpackId)).Click();
+            IWebElement cartItemCount = this.driver.FindElement(By.XPath(shoppingCartBadgeMessageXPath));
+            Assert.That(cartItemCount.Text, Is.EqualTo("1"), "The number of items is different then expected.");
+        }
+        [Test]
+
+        public void TestAddingMultipleItemsInCart()
+        {
+
+            this.driver.FindElement(By.Id(buttonForAddingItemSauceLabsBackpackId)).Click();
+            this.driver.FindElement(By.Id("add-to-cart-sauce-labs-bike-light")).Click();
+            this.driver.FindElement(By.Id("add-to-cart-sauce-labs-bolt-t-shirt")).Click();
+            IWebElement cartItemCount = this.driver.FindElement(By.XPath(shoppingCartBadgeMessageXPath));
+            Assert.That(cartItemCount.Text, Is.EqualTo("3"), "The number of items is different then expected.");
+            cartItemCount.Click();
+            ReadOnlyCollection<IWebElement> cartListOfAddedItems = this.driver.FindElements(By.XPath("//div[@class='cart_item']"));
+            Assert.That(cartListOfAddedItems.Count, Is.EqualTo(3), "The number of items is different then expected.");
+
+        }
+        [Test]
+
+        public void TestRemovingItemFromTheCart()
+        {
+            this.driver.FindElement(By.Id(buttonForAddingItemSauceLabsBackpackId)).Click();
+            IWebElement cartItemCount = this.driver.FindElement(By.XPath(shoppingCartBadgeMessageXPath));
+            Assert.That(cartItemCount.Text, Is.EqualTo("1"), "You have different number of items in cart!");
+            this.driver.FindElement(By.Id("remove-sauce-labs-backpack")).Click();
+            IWebElement cartItemIcon = this.driver.FindElement(By.XPath("//div[@id='shopping_cart_container']/a[@class='shopping_cart_link']"));
+            Assert.That(cartItemIcon.Text, Is.EqualTo(""), "You have different number of items in cart!");
+            cartItemIcon.Click();
+            ReadOnlyCollection<IWebElement> cartListOfAddedItems = this.driver.FindElements(By.XPath("//div[@class='cart_item']"));
+            Assert.That(cartListOfAddedItems.Count, Is.EqualTo(0), "The number of items is different then expected.");
+
+        }
+    }
+}
---
+++ Selenium/TaskForInterview/TaskForInterview/LogInPageTest.cs
@@ -0,0 +1,109 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TaskForInterview
+{
+    [TestFixture] 
+    internal class LogInPageTest
+    {
+        public  string url = "https://www.saucedemo.com/";
+        public IWebDriver driver;
+        //elements
+        public string usernameId = "user-name";
+        public string passwordId = "password";
+        public string loginButtonId = "login-button";
+        public string errorMessageXPath = "//div[@class='error-message-container error']/h3[@data-test='error']"; //I would create ticket for adding Id for this element.
+
+        //users
+
+        public string usernameStandardUser = "standard_user";
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--incognito");
+            this.driver = new ChromeDriver(options);
+            this.driver.Manage().Window.Maximize();
+            this.driver.Navigate().GoToUrl(url);
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            this.driver.Close();
+        }
+
+
+        [Test]
+        public void TestLogInWithoutUsernameAndPassword()
+        {
+            this.driver.FindElement(By.Id(loginButtonId)).Click();
+            IWebElement errorMessageElement = this.driver.FindElement(By.XPath(errorMessageXPath));
+            Assert.That(errorMessageElement.Text, Is.EqualTo ("Epic sadface: Username is required"), "Expected error message is not dispalyed.");
+        }
+
+        [Test]
+        public void TestLoginWithValidUserNameAndNoPassword()
+        {
+
+            this.driver.FindElement(By.Id(usernameId)).SendKeys(usernameStandardUser);
+            this.driver.FindElement(By.Id(loginButtonId)).Click();
+            IWebElement errorMessageElement = this.driver.FindElement(By.XPath(errorMessageXPath));
+            Assert.That(errorMessageElement.Text, Is.EqualTo("Epic sadface: Password is required"), "Expected error message is not dispalyed.");
+
+        }
+
+
+        [Test]
+        public void TestLoginWithInvalidUsername()
+        {
+
+            this.driver.FindElement(By.Id(usernameId)).SendKeys("Invalid Username");
+            this.driver.FindElement(By.Id(passwordId)).SendKeys("secret_sauce");
+            this.driver.FindElement(By.Id(loginButtonId)).Click();
+            IWebElement errorMessageElement = this.driver.FindElement(By.XPath(errorMessageXPath));
+            Assert.That(errorMessageElement.Text, Is.EqualTo("Epic sadface: Username and password do not match any user in this service"), "Expected error message is not dispalyed.");
+
+        }
+
+
+        [Test]
+        public void TestLoginWithInvalidPassword()
+        {
+            
+            this.driver.FindElement(By.Id(usernameId)).SendKeys(usernameStandardUser);
+            this.driver.FindElement(By.Id(passwordId)).SendKeys("Invalid password");
+            this.driver.FindElement(By.Id(loginButtonId)).Click();
+            IWebElement errorMessageElement = this.driver.FindElement(By.XPath(errorMessageXPath));
+            Assert.That(errorMessageElement.Text, Is.EqualTo("Epic sadface: Username and password do not match any user in this service"), "Expected error message is not dispalyed.");
+
+        }
+
+        [TestCase("standard_user")]
+        [TestCase("problem_user")]
+        [TestCase("performance_glitch_user")]
+        [TestCase("error_user")]
+        [TestCase("visual_user")]
+       
+        public void TestValidLogIn(string username)
+        {
+
+            this.driver.FindElement(By.Id(usernameId)).SendKeys(username);
+            this.driver.FindElement(By.Id(passwordId)).SendKeys("secret_sauce");
+            this.driver.FindElement(By.Id(loginButtonId)).Click();
+            string currentURL = this.driver.Url;
+            Assert.That(currentURL, Is.EqualTo("https://www.saucedemo.com/inventory.html"), "URL is not correct.");
+
+        }
+        [Test]
+
+        public void TestLoginWithLockedOutUser()
+        {
+            this.driver.FindElement(By.Id(usernameId)).SendKeys("locked_out_user");
+            this.driver.FindElement(By.Id(passwordId)).SendKeys("secret_sauce");
+            this.driver.FindElement(By.Id(loginButtonId)).Click();
+            var errorMessageElement = this.driver.FindElement(By.XPath(errorMessageXPath));
+            Assert.That(errorMessageElement.Text, Is.EqualTo("Epic sadface: Sorry, this user has been locked out."), "Expected error message is not dispalyed.");
+        }
+    }
+}
